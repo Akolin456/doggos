84458b8f535c3fdf5e709cd03e6d849c08244b9a
Akolin456 <jewelmallik2001@gmail.com>
2024-03-28 02:24:05 +0530

    done image viewer and conditional rendering

--- components/common/ImageViewer/ImageViewer.js
+++ components/common/ImageViewer/ImageViewer.js
@@ -1,7 +1,59 @@
 import Styles from "./ImageViewer.module.css";
+import Image from "next/image";
+import ImageView from "../../../public/Assets/Image.png";
+import Minimize from "../../../public/Assets/Minimize.png";
+import Maximize from "../../../public/Assets/Maximize.png";
+import Exit from "../../../public/Assets/Exit.png";
+import Image1 from "../../../public/Assets/image1.jpg";
+import { useHomePageContext } from "@/contexts/HomePageContext";
 
 const ImageViewer = () => {
-  return <div>this is ImageViewer</div>;
+  const {
+    imageViewerState,
+    imageObj,
+    imageName,
+    SetfolderState,
+    SetimageViewerState,
+  } = useHomePageContext();
+
+  const handleExit = () => {
+    SetimageViewerState(false);
+    SetfolderState(true);
+  };
+
+  if (imageViewerState)
+    return (
+      <div className={Styles.ImageViewerBackground}>
+        <div className={Styles.navBar}>
+          <div className={Styles.folderNameContainer}>
+            <Image src={ImageView} height={30} width={35} />
+            <div className={Styles.folderName}>{imageName}</div>
+          </div>
+          <div className={Styles.navigation}>
+            <Image
+              src={Minimize}
+              height={60}
+              style={{ background: "transparent", cursor: "pointer" }}
+            />
+            <Image
+              src={Maximize}
+              height={60}
+              style={{ background: "transparent", cursor: "pointer" }}
+            />
+            <Image
+              src={Exit}
+              height={60}
+              style={{ background: "transparent", cursor: "pointer" }}
+              onClick={handleExit}
+            />
+          </div>
+        </div>
+        <div className={Styles.body}>
+          <Image src={imageObj} height={812} width={1904} />
+          {/* {console.log(imageValue, "imagevalue")} */}
+        </div>
+      </div>
+    );
 };
 
 export default ImageViewer;
