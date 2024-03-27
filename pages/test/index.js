50db0111a4bc55e89cc115ce6208115b69cefd31
Akolin456 <jewewlmallik2001@gmail.com>
2024-03-26 23:38:19 -0700

    folder

--- pages/test/index.js
+++ pages/test/index.js
@@ -1,9 +1,11 @@
 import Styles from "./test.module.css";
 import Menu from "@/components/HomePageComponents/Menu/Menu";
+import Folder from "@/components/common/Folder/Folder.Components";
 const Test = () => {
   return (
     <div className={Styles.testContainer}>
-      <Menu />
+      {/* <Menu /> */}
+      <Folder />
     </div>
   );
 };
